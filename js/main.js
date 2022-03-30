c7fa2c5f29a46a2128a9e08752c8027d6105f7bd
skaska1x1 <skaska1x1@ya.ru>
2022-03-30 21:11:07 +0300

    scroll

--- js/main.js
+++ js/main.js
@@ -2,7 +2,27 @@ let burger = document.querySelector('.header__burger');
 let headerNav = document.querySelector('.header__nav');
 
 burger.addEventListener('click', function () {
-    // headerNav.style = "transform: translateY(0%);"
-    // burger.classList.toggle('active')
-    headerNav.classList.toggle('active')
+    headerNav.classList.toggle('active');
 })
+
+let btnScrollDown = document.querySelector('#header__scroll-svg');
+
+// btnScrollDown.addEventListener('click', function () {
+//     window.scrollTo(0, 1400);
+// });
+
+function scrollDown() {
+    let windowCoords = 1400;
+    // let windowCoords = document.documentElement.clientHeight;
+    (function scroll() {
+        if (window.scrollY < windowCoords) {
+            window.scrollBy(0, 10);
+            setTimeout(scroll, 0);
+        }
+        // if (window.pageYOffset > windowCoords) {
+        //     window.scrollTo(0, windowCoords);
+        // }
+    })();
+}
+
+btnScrollDown.addEventListener('click', scrollDown);
